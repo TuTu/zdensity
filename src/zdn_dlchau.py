0ce89a31fc9c675070c51113824c8b42b77243fc
KM Tu <tutu522@gmail.com>
2011-06-26 18:29:13 +0900

    In progress

--- src/zdn_dlchau.py
+++ src/zdn_dlchau.py
@@ -2,32 +2,42 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Calculate z-density')
-parser.add_argument('atom_name', 
-    help='the atom whose z-density is to be calculated')
-parser.add_argument('history', nargs='+', 
-    type=argparse.FileType('r'),
+parser.add_argument('-a', '--atom', nargs='+', required=True,
+    help='the atom(s) whose z-density are to be calculated')
+parser.add_argument('-d', '--history', nargs='+', required=True,
+    type=argparse.FileType('r'), metavar='HISTORY',
     help='HISTORY files of DL_POLY in Chau\'s version')
+parser.add_argument('-r', '--atqref', default='ATQREF',
+    type=argparse.FileType('r'), 
+    help='ATQREF file for referencing the atom records')
 
 args = parser.parse_args()
-print(vars(args))
-#target = sys.argv[sys.argv.index("-a") + 1]
-#historyList = sys.argv[sys.argv.index("-f") + 1 : ]
-#for arg in historyList:
-#    if arg[0] == '-': 
-#histories = [open(history, 'r') for history in historyList]
+print("atom: " + str(args.atom))
+atqrefList= [line.split()[0] for line in args.atqref]
+atom = {}
+for atm in args.atom:
+    atom[atm] = {}
+    atom[atm]['index'] = [i for i in range(len(atqrefList)) if atqrefList[i] == atom]
+    atom[atm]['data'] = []
+#    atomIndex[atom] = [i for i in range(len(atqrefList)) if atqrefList[i] == atom ]
 
-#print("Target atom: " + target)
-#print("HISTORY files: " + str(historyList))
-#
-#atqref = open(r'ATQREF', 'r')
-#atmlist = [line.split()[0] for line in atqref]
-#targetIndex = [i for i in range(len(atmlist)) if atmlist[i] == target]
+print(atom['index'])
 
-lineCounter = 0
 for file in args.history:
-    print(file.readline(), end='')
-#  for line in file:
-#    if lineCounter == 0: 
-#        line.split()[0]
-#  lineCounter += 1
+    lineCounter = 0
+    for line in file:
+        if lineCounter == 0: 
+            if not int(line.split()[3]) == len(atqrefList):
+                raise SystemExit("Total number of atoms does not coherent!\n"
+                    "in " file.name ": " + line.split()[3]
+                    "in " atqref.name ": " + str(len(atqrefList)))
+            frame = line.split()[0]
+        elif 2 <= lineCounter <= 4:
+            index = lineCounter - 2
+            dim[index] = [float(line.split()[index])] 
+        elif lineCounter >= 10
+            atomCounter = (lineCounter - 10) % (len(atqrefList) + 3)
+            for atm in args.atom:
+                if atomCounter in atomIndex[atom]
+        lineCounter += 1
 
